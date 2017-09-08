f6815b465864db4e22176090b5e0b1e24785c833
taojy123 <taojy123@163.com>
2017-09-08 15:42:57 +0800

    1

--- chess/models.py
+++ chess/models.py
@@ -265,9 +265,9 @@ class Game(models.Model):
 
                             print d1, d2
 
-                            if d1 < d2:
+                            if d1 > d2:
                                 return i1, j1
-                            elif d1 > d2:
+                            elif d1 < d2:
                                 return i2, j2
                             else:
                                 return i, j
